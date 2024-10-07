d71890c9896269c9eeeace0ba2f2aaebda718874
Smile1-cyber <prodalas999@gmail.com>
2024-10-07 17:46:51 +0300

    Paraliax

--- Assets/Scripts/Fly.cs
+++ Assets/Scripts/Fly.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 [RequireComponent(typeof(Rigidbody2D))]
 public class Fly : MonoBehaviour
 {
     public float jumpForce = 10;
+    public TextMeshProUGUI scoreText;
 
     private Rigidbody2D rb;
+    private int points = 0;
 
     private void Start() 
     {
@@ -36,5 +38,10 @@ public class Fly : MonoBehaviour
         {
             transform.rotation = Quaternion.Euler(0, 0, -30);
         }
+    }
+
+   private void OnTriggerExit2D(Collision2D other)
+    {
+        scoreText.text = (++points).ToString("D4");
     }
 }
